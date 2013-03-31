548648338b9c4da2b263651942e404d56ce3a646
HU Pili <hpl1989@gmail.com>
2013-04-01 00:33:10 +0800

    restructure and decorate all rlq methods

--- wauto.py
+++ wauto.py
@@ -2,14 +2,69 @@
 
 import sys
 sys.path.append('snsaspi')
+import copy
+
 import snsapi
 from snsapi.snspocket import SNSPocket
 from snsapi.snslog import SNSLog as logger
 from lbucket import *
 
+
+def cal_bucket(upperbound, period):
+    '''
+    Do conservative calculation to generate a bucket for SinaWeibo
+
+    The limit is represented as ``x/y`` in the doc. 
+
+    :param upperbound: x (in original scale)
+    :param period: y (in seconds)
+    '''
+    # for safety
+    ratio_capacity = 0.8
+    ratio_init = 0.1
+    upperbound = float(upperbound)
+    period = float(period)
+    fill_rate = upperbound / period
+    tokens_max = upperbound * 0.8
+    tokens_init = upperbound * 0.1
+    return LeakyBucket(tokens_max, tokens_init, fill_rate)
+
+
 class WeiboAutomator(object):
     '''Wrap common operations with rate limit facility
     '''
+
+    # Most buckets are derived from Sina's offcial description [1]. 
+    # The additional one 'wauto_snsapi' limits SNSAPI request rate globally
+    # (avoid lower layer failure). 
+    # 
+    # Ref:
+    #    * [1] http://open.weibo.com/wiki/Rate-limiting
+    SINA_BUCKETS = [
+            ('wauto_snsapi', LeakyBucket(1, 1, 1)),
+            ('ip.hour.test_auth', cal_bucket(1000, 60*60)),
+            ('user.hour.test_auth.total', cal_bucket(150, 60*60)),
+            ('user.hour.test_auth.update', cal_bucket(30, 60*60)),
+            ('user.hour.test_auth.reply', cal_bucket(60, 60*60)),
+            ('user.hour.test_auth.follow', cal_bucket(60, 60*60)),
+            ('user.day.test_auth.follow', cal_bucket(100, 60*60*24)),
+            ]
+
+    POLICY_GROUP = {}
+    POLICY_GROUP['general'] = {
+            'wauto_snsapi': 1, 
+            'ip.hour.test_auth': 1,
+            'user.hour.test_auth.total': 1
+            }
+    POLICY_GROUP['update'] = dict(POLICY_GROUP['general'], 
+            **{'user.hour.test_auth.update': 1})
+    POLICY_GROUP['reply'] = dict(POLICY_GROUP['general'], 
+            **{'user.hour.test_auth.reply': 1})
+    POLICY_GROUP['follow'] = dict(POLICY_GROUP['general'], 
+            **{'user.hour.test_auth.follow': 1, 'user.day.test_auth.follow': 1})
+    
+    _log = lambda x: logger.debug('ret: %s', x)
+
     def __init__(self):
         super(WeiboAutomator, self).__init__()
         self.sp = SNSPocket()
@@ -18,65 +73,35 @@ class WeiboAutomator(object):
         # assign 'channel_name' as automator
         self.weibo = self.sp['automator'] 
 
-        # Most buckets are derived from Sina's offcial description [1]. 
-        # The additional one 'wauto_snsapi' limits SNSAPI request rate globally
-        # (avoid lower layer failure). 
-        # 
-        # Ref:
-        #    * [1] http://open.weibo.com/wiki/Rate-limiting
-        sina_buckets = [
-                ('wauto_snsapi', LeakyBucket(1, 1, 1)),
-                ('ip.hour.test_auth', self.cal_bucket(1000, 60*60)),
-                ('user.hour.test_auth.total', self.cal_bucket(150, 60*60)),
-                ('user.hour.test_auth.update', self.cal_bucket(30, 60*60)),
-                ('user.hour.test_auth.reply', self.cal_bucket(60, 60*60)),
-                ('user.hour.test_auth.follow', self.cal_bucket(60, 60*60)),
-                ('user.day.test_auth.follow', self.cal_bucket(100, 60*60*24)),
-                ]
         self.rlq = RateLimitQueue() 
-        map(lambda t: self.rlq.add_bucket(t[0], t[1]), sina_buckets)
-
-    def cal_bucket(self, upperbound, period):
-        '''
-        Do conservative calculation to generate a bucket for SinaWeibo
-
-        The limit is represented as ``x/y`` in the doc. 
-
-        :param upperbound: x (in original scale)
-        :param period: y (in seconds)
-        '''
-        # for safety
-        ratio_capacity = 0.8
-        ratio_init = 0.1
-        upperbound = float(upperbound)
-        period = float(period)
-        fill_rate = upperbound / period
-        tokens_max = upperbound * 0.8
-        tokens_init = upperbound * 0.1
-        return LeakyBucket(tokens_max, tokens_init, fill_rate)
+        map(lambda t: self.rlq.add_bucket(t[0], t[1]), self.SINA_BUCKETS)
 
     def run(self):
         return self.rlq.run()
 
-
+    @rate_limit(buckets=POLICY_GROUP['follow'], callback=_log)
     def follow(self, uid):
         ret = self.weibo.weibo_request('friendships/create',
                 'POST',
                 {'uid': uid})
         return ret
 
+    @rate_limit(buckets=POLICY_GROUP['follow'], callback=_log)
     def follow_by_name(self, screen_name):
         ret = self.weibo.weibo_request('friendships/create',
                 'POST',
                 {'screen_name': screen_name})
         return ret
 
+    @rate_limit(buckets=POLICY_GROUP['general'], callback=_log)
     def home_timeline(self, count=20):
         return self.weibo.home_timeline(count)
 
+    @rate_limit(buckets=POLICY_GROUP['update'], callback=_log)
     def update(self, text):
         return self.weibo.update(text)
 
+    @rate_limit(buckets=POLICY_GROUP['reply'], callback=_log)
     def reply(self, status, text):
         if isinstance(status, snsapi.snstype.Message):
             statusID = status.ID
@@ -84,9 +109,11 @@ class WeiboAutomator(object):
             statusID = status
         return self.weibo.reply(statusID, text)
 
+    @rate_limit(buckets=POLICY_GROUP['general'], callback=_log)
     def forward(self, status, text):
         return self.weibo.forward(status, text)
 
+    @rate_limit(buckets=POLICY_GROUP['general'], callback=_log)
     def domain_show(self, url):
         '''Lookup user by personal url. 
         We will match and remove common weibo prefix. 
