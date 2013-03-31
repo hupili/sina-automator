0451fa62c697156ff75843e5d7bcf6f50f5eacf4
HU Pili <hpl1989@gmail.com>
2013-03-31 15:29:58 +0800

    up test case: hit the capacity

--- test_lbucket.py
+++ test_lbucket.py
@@ -2,16 +2,19 @@
 
 from lbucket import LeakyBucket
 
-if __name__ == '__main__':
+def test_bucket():
     from time import sleep
-    bucket = LeakyBucket(80, 60, 1)
+    bucket = LeakyBucket(80, 70, 10)
     print "tokens =", bucket.tokens
-    print "consume(10) =", bucket.consume(10)
-    print "consume(10) =", bucket.consume(10)
+    print "consume(10) =", bucket.consume(8)
+    print "consume(10) =", bucket.consume(1)
     sleep(1)
     print "tokens =", bucket.tokens
     sleep(1)
     print "tokens =", bucket.tokens
     print "consume(90) =", bucket.consume(90)
     print "tokens =", bucket.tokens
+
+if __name__ == '__main__':
+    test_bucket()
 
